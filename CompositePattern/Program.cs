01f6b74b4cdb771194bb01d95c4f36bf52acdcef
mark.chua03@gmail.com <mark.chua03@gmail.com>
2016-04-05 00:20:41 +0800

    dofactory

--- CompositePattern/Program.cs
+++ CompositePattern/Program.cs
@@ -1,155 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-namespace CompositePattern
+namespace DoFactory.GangOfFour.Composite.Structural
 {
-    // The Interface
-    public interface IComponent<T>
+
+    /// <summary>
+    /// The 'Component' abstract class
+    /// </summary>
+    abstract class Component
     {
-        void Add(IComponent<T> c);
-        IComponent<T> Remove(T s);
-        string Display(int depth);
-        IComponent<T> Find(T s);
-        T Name { get; set; }
+        protected string name;
+
+        // Constructor
+        public Component(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract void Add(Component c);
+        public abstract void Remove(Component c);
+        public abstract void Display(int depth);
     }
-    // The Composite
-    public class Composite<T> : IComponent<T>
+
+    /// <summary>
+    /// The 'Composite' class
+    /// </summary>
+    class Composite : Component
     {
-        List<IComponent<T>> list = null;
+        private List<Component> _children = new List<Component>();
 
-        public T Name { get; set; }
-
-        public Composite(T name)
+        // Constructor
+        public Composite(string name)
+          : base(name)
         {
-            Name = name;
-            list = new List<IComponent<T>>();
         }
 
-        public void Add(IComponent<T> c)
+        public override void Add(Component component)
         {
-            list.Add(c);
+            _children.Add(component);
         }
 
-        // Finds the item from a particular point in the structure
-        // and returns the composite from which it was removed
-        // If not found, return the point as given
-        public IComponent<T> Remove(T s)
+        public override void Remove(Component component)
         {
-            holder = this;
-            IComponent<T> p = holder.Find(s);
-            if (holder != null)
+            _children.Remove(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+
+            // Recursively display child nodes
+            foreach (Component component in _children)
             {
-                (holder as Composite<T>).list.Remove(p);
-                return holder;
+                component.Display(depth + 2);
             }
-            else
-                return this;
-        }
-
-        IComponent<T> holder = null;
-
-        // Recursively looks for an item
-        // Returns its reference or else null
-        public IComponent<T> Find(T s)
-        {
-            holder = this;
-            if (Name.Equals(s)) return this;
-            IComponent<T> found = null;
-            foreach (IComponent<T> c in list)
-            {
-                found = c.Find(s);
-                if (found != null)
-                    break;
-            }
-            return found;
-        }
-
-        // Displays items in a format indicating their level in the composite structure
-        public string Display(int depth)
-        {
-            StringBuilder s = new StringBuilder(new String('-', depth));
-            s.Append("Set " + Name + " length :" + list.Count + "\n");
-            foreach (IComponent<T> component in list)
-            {
-                s.Append(component.Display(depth + 2));
-            }
-            return s.ToString();
         }
     }
 
-    // The Component
-    public class Component<T> : IComponent<T>
+    /// <summary>
+    /// The 'Leaf' class
+    /// </summary>
+    class Leaf : Component
     {
-        public T Name { get; set; }
-
-        public Component(T name)
+        // Constructor
+        public Leaf(string name)
+          : base(name)
         {
-            Name = name;
         }
 
-        public void Add(IComponent<T> c)
+        public override void Add(Component c)
         {
-            Console.WriteLine("Cannot add to an item");
+            Console.WriteLine("Cannot add to a leaf");
         }
 
-        public IComponent<T> Remove(T s)
+        public override void Remove(Component c)
         {
-            Console.WriteLine("Cannot remove directly");
-            return this;
+            Console.WriteLine("Cannot remove from a leaf");
         }
 
-        public string Display(int depth)
+        public override void Display(int depth)
         {
-            return new String('-', depth) + Name + "\n";
-        }
-
-        public IComponent<T> Find(T s)
-        {
-            if (s.Equals(Name))
-                return this;
-            else
-                return null;
+            Console.WriteLine(new String('-', depth) + name);
         }
     }
 
-    class Program
+
+    /// <summary>
+    /// MainApp startup class for Structural 
+    /// Composite Design Pattern.
+    /// </summary>
+    class MainApp
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
         {
-            IComponent<string> album = new Composite<string>("Album");
-            IComponent<string> point = album;
-            string[] s;
-            string command, parameter;
-            // Create and manipulate a structure 
-            StreamReader instream = new StreamReader("composite.dat");
-            do
-            {
-                string t = instream.ReadLine();
-                Console.WriteLine("\t\t\t\t" + t);
-                s = t.Split();
-                command = s[0];
-                if (s.Length > 1) parameter = s[1]; else parameter = null;
-                switch (command)
-                {
-                    case "AddSet":
-                        IComponent<string> c = new Composite<string>(parameter);
-                        point.Add(c);
-                        point = c;
-                        break;
-                    case "AddPhoto": point.Add(new Component<string>(parameter)); break;
-                    case "Remove": point = point.Remove(parameter); break;
-                    case "Find": point = album.Find(parameter); break;
-                    case "Display": Console.WriteLine(album.Display(0)); break;
-                    case "Quit": break;
-                }
+            // Create a tree structure
+            Composite root = new Composite("root");
+            root.Add(new Leaf("Leaf A"));
+            root.Add(new Leaf("Leaf B"));
 
-            } while (!command.Equals("Quit"));
+            Composite comp = new Composite("Composite X");
+            comp.Add(new Leaf("Leaf XA"));
+            comp.Add(new Leaf("Leaf XB"));
 
+            root.Add(comp);
+            root.Add(new Leaf("Leaf C"));
+
+            // Add and remove a leaf
+            Leaf leaf = new Leaf("Leaf D");
+            root.Add(leaf);
+            root.Remove(leaf);
+
+            // Recursively display tree
+            root.Display(1);
+
+            // Wait for user
             Console.ReadKey();
         }
     }
