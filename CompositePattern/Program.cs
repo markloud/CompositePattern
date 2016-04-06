fab6dce1fe561f16abd63e22d690a66a0fb3c3a0
mark.chua03@gmail.com <mark.chua03@gmail.com>
2016-04-06 09:29:40 +0800

    conform with SOLID

--- CompositePattern/Program.cs
+++ CompositePattern/Program.cs
@@ -3,88 +3,6 @@ using System.Collections.Generic;
 
 namespace DoFactory.GangOfFour.Composite.Structural
 {
-
-    /// <summary>
-    /// The 'Component' abstract class
-    /// </summary>
-    abstract class Component
-    {
-        protected string name;
-
-        // Constructor
-        public Component(string name)
-        {
-            this.name = name;
-        }
-
-        public abstract void Add(Component c);
-        public abstract void Remove(Component c);
-        public abstract void Display(int depth);
-    }
-
-    /// <summary>
-    /// The 'Composite' class
-    /// </summary>
-    class Composite : Component
-    {
-        private List<Component> _children = new List<Component>();
-
-        // Constructor
-        public Composite(string name)
-          : base(name)
-        {
-        }
-
-        public override void Add(Component component)
-        {
-            _children.Add(component);
-        }
-
-        public override void Remove(Component component)
-        {
-            _children.Remove(component);
-        }
-
-        public override void Display(int depth)
-        {
-            Console.WriteLine(new String('-', depth) + name);
-
-            // Recursively display child nodes
-            foreach (Component component in _children)
-            {
-                component.Display(depth + 2);
-            }
-        }
-    }
-
-    /// <summary>
-    /// The 'Leaf' class
-    /// </summary>
-    class Leaf : Component
-    {
-        // Constructor
-        public Leaf(string name)
-          : base(name)
-        {
-        }
-
-        public override void Add(Component c)
-        {
-            Console.WriteLine("Cannot add to a leaf");
-        }
-
-        public override void Remove(Component c)
-        {
-            Console.WriteLine("Cannot remove from a leaf");
-        }
-
-        public override void Display(int depth)
-        {
-            Console.WriteLine(new String('-', depth) + name);
-        }
-    }
-
-
     /// <summary>
     /// MainApp startup class for Structural 
     /// Composite Design Pattern.
@@ -118,6 +36,74 @@ namespace DoFactory.GangOfFour.Composite.Structural
 
             // Wait for user
             Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Component' abstract class
+    /// </summary>
+    abstract class Component // removed methods not used by component to conform with SOLID
+    {
+        protected string name;
+
+        // Constructor
+        public Component(string name)
+        {
+            this.name = name;
+        }
+        
+        public abstract void Display(int depth);
+    }
+
+    /// <summary>
+    /// The 'Composite' class
+    /// </summary>
+    class Composite : Component
+    {
+        private List<Component> _children = new List<Component>();
+
+        // Constructor
+        public Composite(string name)
+          : base(name)
+        {
+        }
+
+        public void Add(Component component)
+        {
+            _children.Add(component);
+        }
+
+        public void Remove(Component component)
+        {
+            _children.Remove(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+
+            // Recursively display child nodes
+            foreach (Component component in _children)
+            {
+                component.Display(depth + 2);
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'Leaf' class
+    /// </summary>
+    class Leaf : Component
+    {
+        // Constructor
+        public Leaf(string name)
+          : base(name)
+        {
+        }
+        
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
         }
     }
 }
